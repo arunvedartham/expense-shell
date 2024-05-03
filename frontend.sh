1375f9124abc2759590fa9583aa5f1fb42b040da
ArunVedartham-Devops <vedarth@gmail.com>
2024-05-03 18:56:58 +0530

    enable colours for headings

--- frontend.sh
+++ frontend.sh
@@ -1,20 +1,20 @@
-echo Installation Nginx
+echo -e "\e[31mInstalling Nginx\e[0m"
 dnf install nginx -y
 
-echo Copy expense config file
+echo -e "\e[32mCopy expense config file\e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf
 
-echo clean old content
+echo -e "\e[33mclean old content\e[0m"
 rm -rf /usr/share/nginx/html/*
 
-echo dowload frontend content
+echo -e "\e[34mdowload frontend content\e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
 cd /usr/share/nginx/html
 
-echo extract content
+echo -e "\e[35mextract content\e[0m"
 unzip /tmp/frontend.zip
 
-echo restart service
+echo -e "\e[36mrestart service\e[0m"
 systemctl restart nginx
 systemctl enable nginx
 
