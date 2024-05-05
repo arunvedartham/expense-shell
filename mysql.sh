88d009e4b4fdc68926035e2801651ea1356fb1df
ArunVedartham-Devops <vedarth@gmail.com>
2024-05-05 16:00:57 +0530

    functions in frotend

--- mysql.sh
+++ mysql.sh
@@ -1,10 +1,11 @@
-
-echo -e "\e[31mInstalling mysql\e[0m"
+source common.sh
+heading Installing mysql
 dnf install mysql-server -y
 
-echo -e "\e[31mEnable and start mysql\e[0m"
+heading Enable and start mysql
 systemctl enable mysqld
 systemctl start mysqld
-echo -e "\e[31mSecure Installing mysql root password\e[0m"
+
+heading Secure Installing mysql root password
 mysql_secure_installation --set-root-pass $1
 # --set-root-pass ExpenseApp@1
