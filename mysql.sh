25108ef32dc1b207ed39d5570d658f978a24cadd
ArunVedartham-Devops <vedarth@gmail.com>
2024-05-05 14:35:13 +0530

    Pass Variable values

--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,10 @@
+
+echo -e "\e[31mInstalling mysql\e[0m"
 dnf install mysql-server -y
+
+echo -e "\e[31mEnable and start mysql\e[0m"
 systemctl enable mysqld
 systemctl start mysqld
-mysql_secure_installation --set-root-pass ExpenseApp@1
+echo -e "\e[31mSecure Installing mysql root password\e[0m"
+mysql_secure_installation --set-root-pass $1
+# --set-root-pass ExpenseApp@1
