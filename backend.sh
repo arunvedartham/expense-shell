4e48587681357bd4ac82dde57de766fb83d54cf1
ArunVedartham-Devops <vedarth@gmail.com>
2024-05-06 10:57:32 +0530

    Conditions test

--- backend.sh
+++ backend.sh
@@ -1,21 +1,21 @@
 source common.sh
 rm -f /tmp/expense.log
 
-HEADING "Disable NodeJS default version"
+heading "Disable NodeJS default version"
 dnf module disable nodejs -y &>>/tmp/expense.log
 STAT $?
 
-HEADING "Enable NodeJS 20"
+heading "Enable NodeJS 20"
 dnf module enable nodejs:20 -y &>>/tmp/expense.log
 STAT $?
 
-HEADING "Install NodeJS"
+heading "Install NodeJS"
 dnf install nodejs -y &>>/tmp/expense.log
 STAT $?
 
 # useradd expense
 
-HEADING "Add Expense User"
+heading "Add Expense User"
 id expense &>>/tmp/expense.log
 if [ $? -ne 0 ]; then
   useradd expense &>>/tmp/expense.log
@@ -24,23 +24,23 @@ STAT $?
 
 # cp backend.service /etc/systemd/system/backend.service
 
-HEADING "Setup Backend Service"
+heading "Setup Backend Service"
 cp backend.service /etc/systemd/system/backend.service &>>/tmp/expense.log
 STAT $?
 
-HEADING "Delete Existing Application Directory"
+heading "Delete Existing Application Directory"
 rm -rf /app
 STAT $?
 
 # mkdir /app
 
-HEADING "Create Application Directory"
+heading "Create Application Directory"
 mkdir /app &>>/tmp/expense.log
 STAT $?
 
 # curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
 
-HEADING "Download Backend Code"
+heading "Download Backend Code"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>/tmp/expense.log
 STAT $?
 
@@ -48,29 +48,29 @@ cd /app
 
 # unzip /tmp/backend.zip
 
-HEADING "Extract Backend code"
+heading "Extract Backend code"
 unzip /tmp/backend.zip &>>/tmp/expense.log
 STAT $?
 
 # npm install
 
-HEADING "Download NodeJS App Dependencies"
+heading "Download NodeJS App Dependencies"
 npm install &>>/tmp/expense.log
 STAT $?
 
 # dnf install mysql -y
 
-HEADING "Install MySQL Client"
+heading "Install MySQL Client"
 dnf install mysql -y &>>/tmp/expense.log
 STAT $?
 
 # mysql -h 172.31.30.152 -uroot -pExpenseApp@1 < /app/schema/backend.sql
 
-HEADING "Load Schema"
+heading "Load Schema"
 mysql -h 172.31.30.152 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>/tmp/expense.log
 STAT $?
 
-HEADING "Start Backend Service"
+heading "Start Backend Service"
 systemctl daemon-reload
 systemctl enable backend
 systemctl restart backend
