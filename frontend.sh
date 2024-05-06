4dc78cc29f3ac14bdafbfe2932a2f20b9c6e79a7
ArunVedartham-Devops <vedarth@gmail.com>
2024-05-06 10:41:55 +0530

    Conditions test

--- frontend.sh
+++ frontend.sh
@@ -1,31 +1,32 @@
 
 source common.sh
+rm -f /tmp/expense.log
 
 heading Installing Nginx
 dnf install nginx -y &>>/tmp/expense.log
-echo exit status - $?
+STAT $?
 
 heading copy expense config file
 cp expense.conf /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
-echo exit status - $?
+STAT $?
 
 heading clean old content
 rm -rf /usr/share/nginx/html/* &>>/tmp/expense.log
-echo exit status - $?
+STAT $?
 
 heading download frontend content
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>>/tmp/expense.log
-echo exit status - $?
+STAT $?
 cd /usr/share/nginx/html
 
 heading Extract content
 unzip /tmp/frontend.zip &>>/tmp/expense.log
-echo exit status - $?
+STAT $?
 
 heading Restart service
 systemctl restart nginx &>>/tmp/expense.log
 systemctl enable nginx &>>/tmp/expense.log
-echo exit status - $?
+STAT $?
 
 
 
