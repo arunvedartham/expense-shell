b162343ea0accda9df7969fa836e088d2fc689c2
arunvedartham <vedarth@gmail.com>
2024-05-08 12:24:54 +0530

    Update backend.sh
    updated to include password if condition statement

--- backend.sh
+++ backend.sh
@@ -1,6 +1,11 @@
 source common.sh
 rm -f /tmp/expense.log
 
+if [ -z "$1" ]; then
+  echo "Input MySQL Password is Missing"
+  exit 1
+fi
+
 heading "Disable NodeJS default version"
 dnf module disable nodejs -y &>>/tmp/expense.log
 STAT $?
